a8d7561863271207ce3adef9ea72aef20dba63d4
Thien <thien.phamchi060200@gmail.com>
2026-05-04 16:45:13 +0700

    feat(mobile): add internal mobile run service

--- mobile_appium/__init__.py
+++ mobile_appium/__init__.py
@@ -1,7 +1,10 @@
 from .driver import MobileTestSettings, create_android_driver, get_mobile_settings
+from .run_service import MobileRunArtifact, MobileRunService
 
 __all__ = [
     "MobileTestSettings",
     "create_android_driver",
     "get_mobile_settings",
+    "MobileRunArtifact",
+    "MobileRunService",
 ]
